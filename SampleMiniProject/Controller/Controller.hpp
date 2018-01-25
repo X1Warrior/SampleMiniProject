491108125dad85ca00fd9efd9833441f881066d9
X1Warrior <zsto7638@gmail.com>
2018-01-25 11:51:53 -0700

    created the controller's cpp and hpp and the runner.cpp

---
+++ SampleMiniProject/Controller/Controller.hpp
@@ -0,0 +1,14 @@
+//
+//  Controller.hpp
+//  SampleMiniProject
+//
+//  Created by Stout, Zachary on 1/25/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <stdio.h>
+
+#endif /* Controller_hpp */
---
+++ SampleMiniProject/Controller/Runner.cpp
@@ -0,0 +1,9 @@
+//
+//  Runner.cpp
+//  SampleMiniProject
+//
+//  Created by Stout, Zachary on 1/25/18.
+//  Copyright © 2018 Stout, Zachary. All rights reserved.
+//
+
+#include <stdio.h>
