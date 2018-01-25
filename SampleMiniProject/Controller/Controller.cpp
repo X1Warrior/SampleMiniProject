94fba99691ea986333e2c0cb827b985d81215d83
X1Warrior <zsto7638@gmail.com>
2018-01-25 12:42:00 -0700

    made a array and a method

--- SampleMiniProject/Controller/Controller.cpp
+++ SampleMiniProject/Controller/Controller.cpp
@@ -22,3 +22,28 @@ void Controller :: start()
         cout << "This is the " << index + 1 << " time in the loop" << endl;
     }
 }
+int addition (int a, int b)
+{
+    int r;
+    r=a+b;
+    return r;
+}
+int oldmain ()
+{
+    int z;
+    z = addition(5,3);
+    cout << "The result is " << z;
+    return z;
+}
+int food [] = {1,3,5,7,9};
+int n, result=0;
+
+int newmain ()
+{
+    for ( n=0 ; n<5 ; ++n )
+    {
+        result += food[n];
+    }
+    cout << result;
+    return 0;
+}
